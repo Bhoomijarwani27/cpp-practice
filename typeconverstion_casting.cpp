220a641ad383f9525cbce18df58ccdcfdaf7a7c2
Bhoomijarwani <bhoomijarwani2006@gmail.com>
2026-02-01 14:35:23 +0530

    Refactor type conversion example with comments
    Added ASCII range comments and adjusted formatting.

--- typeconverstion_casting.cpp
+++ typeconverstion_casting.cpp
@@ -6,7 +6,9 @@ int main(){
 char grade = 'a';
 int value = grade;
 cout << value <<'\n'; // print the value of a in no 
-return 0;     
+return 0;    
+    // ASCLL RANGE 
 // range of a to z in lower case is 97 to 112
 // range of upper A TO Z is 65 to 90
 }
+
